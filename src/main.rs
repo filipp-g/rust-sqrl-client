e53e37adba79b66f2ffa4dfcb12a8fd288311a55
Filipp G <8738425+filipp-g@users.noreply.github.com>
2021-02-14 13:39:14 -0500

    add minimal http library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// use std::collections::HashMap;
+
+const TEST_URL: &str = "http://sqrl.grc.com/cli.sqrl?nut=jLUOj4v1HsZm&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vZGVtbw";
+
+fn main() -> Result<(), ureq::Error>  {
+    let body: String = ureq::get("http://www.example.com/")
+        .set("Example-Header", "header value")
+        .call()?
+        .into_string()?;
+    println!("{:?}", body);
+    Ok(())
 }
