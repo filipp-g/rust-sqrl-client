c73a99ffea024796126326664a903458dab80dcf
Filipp G <8738425+filipp-g@users.noreply.github.com>
2021-04-05 19:32:05 -0400

    refactor into modules and complete 2nd path

---
+++ src/http.rs
@@ -0,0 +1,141 @@
+use tiny_http::{Response, Server};
+
+fn start_server() {
+    let server = Server::http("0.0.0.0:25519").unwrap();
+
+    for request in server.incoming_requests() {
+        println!("received request! method: {:?}, url: {:?}", request.method(), request.url());
+
+        let response = Response::from_string("www.google.com")
+            .with_status_code(301);
+        // .with_header(Header::from_bytes("Location", "www.google.com").unwrap());
+
+        request.respond(response);
+        // exit(0);
+    }
+}
+
+//Parsing function to get back just the 'nut' portion of the URL
+fn parse_nut(url: &str) -> String
+{
+    let mut nut: String = String::from("");
+    if url.starts_with("sqrl://")
+    {
+        let mut temp_url = url.strip_prefix("sqrl://").unwrap();
+
+        temp_url = temp_url.split("?").collect::<Vec<&str>>()[1];
+
+        temp_url = temp_url.split("nut=").collect::<Vec<&str>>()[1];
+
+        temp_url = temp_url.split("&can=").collect::<Vec<&str>>()[0];
+
+        nut.push_str(temp_url);
+
+        return nut;
+    } else {
+        nut.push_str("Error");
+        return nut;
+    }
+}
+
+fn test_parse_nut()
+{
+    let url = "sqrl://sqrl.grc.com/cli.sqrl?nut=jLUOj4v1HsZm&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vZGVtbw";
+    assert_eq!("jLUOj4v1HsZm", parse_nut(&url));
+}
+
+// Parsing function to get back just the domain portion of the URL
+// the exact domain is required to compute the per site private key
+// we can fix formatting on the fnxn to get rid of empty lines, I just like to have my code spread
+// out and separated so its easy to read
+pub fn parse_domain(url: &str) -> String
+{
+    // url param is the SQRL url that looks like: sqrl://example.com/jimbo/?x=6&nut=...
+    // need to parse out the "example.com/jimbo"
+
+    if url.starts_with("sqrl://")
+    {
+        let mut domain: String = String::from("");
+        let mut temp_url = url.strip_prefix("sqrl://").unwrap();
+        let offset = determine_offset(temp_url.split("?").collect::<Vec<&str>>()[1]);
+
+        temp_url = temp_url.split("?").collect::<Vec<&str>>()[0];
+
+        if temp_url.find("@") != None
+        {
+            // keep things to the right of the @ symbol
+            temp_url = temp_url.split("@").collect::<Vec<&str>>()[1];
+        }
+
+        // domain ends at the beginning of the first '/' symbol, eg example.com/
+        let mut end_index = temp_url.find("/").unwrap() as u8;
+        // unless theres an extension, which is then given by the offset var
+        let mut index = 0;
+        for character in temp_url.chars()
+        {
+            if index == (end_index + offset) || character == '?'
+            {
+                break;
+            } else if index < end_index && (character.is_alphabetic() || character == '.')
+            {
+                domain.push(character.to_ascii_lowercase());
+            } else if index >= end_index && index < (end_index + offset)
+            {
+                domain.push(character);
+            }
+
+            index += 1;
+        }
+
+        return domain;
+    } else {
+        return String::from("Error");
+    }
+}
+
+//Helper function for the parsers
+fn determine_offset(params: &str) -> u8
+{
+    // given something like
+    // x=5&nut=oOB4QOFJux5Z&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vYWNjb3VudC9jb25uZWN0ZWQtYWNjb3VudHMv
+
+    if params.get(0..1).unwrap() == "x"
+    {
+        let mut num_str = params.split("&").collect::<Vec<&str>>()[0];
+        num_str = num_str.split("=").collect::<Vec<&str>>()[1];
+
+        return num_str.parse::<u8>().unwrap();
+    }
+    return 0;
+}
+
+
+fn test_parse_domain()
+{
+    // test cases taken from GRCs SQRL Operating Details pg 7
+    // to lowercase
+    assert_eq!(String::from("example.com"), parse_domain("sqrl://ExAmPlE.cOm/?nut="));
+    // removing specified port num
+    assert_eq!(String::from("example.com"), parse_domain("sqrl://example.com:44344/?nut="));
+    // removing username prefix
+    assert_eq!(String::from("example.com"), parse_domain("sqrl://jonny@example.com/?nut="));
+    // removing username:pass prefix
+    assert_eq!(String::from("example.com"), parse_domain("sqrl://Jonny:Secret@example.com/?nut="));
+    // keeping extended auth domain
+    assert_eq!(String::from("example.com/jimbo"), parse_domain("sqrl://example.com/jimbo/?x=6&nut="));
+    // stopping at ? and only making domain lowercase, not extended auth
+    assert_eq!(String::from("example.com/JIMBO"), parse_domain("sqrl://EXAMPLE.COM/JIMBO?x=16&nut="));
+
+    assert_eq!(String::from("sqrl.grc.com/demo"), parse_domain("sqrl://steve:badpass@SQRL.grc.com:8080/demo/cli.sqrl?x=5&nut=oOB4QOFJux5Z&
+can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vYWNjb3VudC9jb25uZWN0ZWQtYWNjb3VudHMv"));
+}
+
+// Sample http request function, will use later
+// fn send_request(url: &str) -> Result<(), ureq::Error> {
+//     let body: String = ureq::get(url)
+//         .set("Example-Header", "header value")
+//         .call()?
+//         .into_string()?;
+//     println!("{:?}", body);
+//     Ok(())
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,52 @@
-use sodiumoxide::crypto::auth;
-use sodiumoxide::crypto::hash::sha256;
-use sodiumoxide::crypto::hash::sha256::Digest;
-use sodiumoxide::crypto::sign;
-use sodiumoxide::crypto::sign::ed25519::{PublicKey, SecretKey};
-use sodiumoxide::randombytes;
 use sodiumoxide::init as sodium_init;
-use sodiumoxide::utils::memzero;
-use noise_sodiumoxide::*;
-use noise_protocol::*;
-use libsodium_sys::*;
-use tiny_http::{Server, Response, Header, StatusCode};
-use std::process::exit;
 use sysinfo::{ProcessExt, SystemExt};
-use std::env;
-use std::ptr::{null_mut, null};
 use text_io::read;
 
-
-
-const TEST_URL: &str = "http://sqrl.grc.com/cli.sqrl?nut=";
-// const TEST_NUT: &str = "jLUOj4v1HsZm&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vZGVtbw";
+mod http;
+mod crypto;
 
 
 fn main() {
-    //Initialize the sodiumoxide library. Makes it thread-safe
-    init();
-    //Test if current CPU supports AES hardware acceleration
-    //this returns true if CPU is supported, false otherwise
-    //println!("{}", Aes256Gcm::available());
-    let mut system = sysinfo::System::new_all();
-    // Need to update all information of system struct
-    system.refresh_all();
-    for (pid, proc_) in system.get_processes() {
-        if proc_.name().starts_with("rust-sqrl-client") {
-            println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
-        }
-    }
-
-
+    // Initialize the sodiumoxide library. Makes it thread-safe
+    sodium_init();
 
     //Loop to get user input and navigate through SQRL implementation
-    let arr: [&str; 4] = ["h","1","2","3"];
+    let arr: [&str; 4] = ["h", "1", "2", "3"];
     loop {
         println!("Enter in a command. Type 'h' for help with commands");
         //Read input from the user
         let mut line: String = read!("{}\n");
         let mut command: bool = false;
         //checks over the array to see if it's a valid command
-        for x in 0..arr.len(){
-            if arr[x] == line{
+        for x in 0..arr.len() {
+            if arr[x] == line {
                 command = true;
             }
         }
         if command {
             handle_command(&*line)
-        }
-        else if line == "0"{
-            break;
-        }
-        else{
+        } else if line == "0" {
+            return;
+        } else {
             println!("Not a valid command. Type 'h' to view list of commands");
         }
     }
 
-
-
-	//added some preliminary code to deal with command line args, if we choose to go that route
-
-	//let mut test_url: String = String::from("");
-
-	// for argument in env::args()
-	// {
-	// 	if argument.starts_with("sqrl://")
-	// 	{
-	// 		test_url = argument;
-	// 	}
-	// }
-
-	// println!("{:?}", test_url);
-
-}
-//Initialize the sodiumoxide library
-pub fn init() -> Result<(), ()> {
-    sodium_init()
+    // // added some preliminary code to deal with command line args, if we choose to go that route
+    // let mut test_url: String = String::from("");
+    //
+    // for argument in env::args()
+    // {
+    //     if argument.starts_with("sqrl://")
+    //     {
+    //         test_url = argument;
+    //     }
+    // }
+    // println!("{:?}", test_url);
 }
 
 //Handles the command input by the user.
-fn handle_command(com: &str){
+fn handle_command(com: &str) {
     if com == "h" {
         println!("LIST OF COMMANDS");
         println!("1 - Create Identity");
@@ -99,239 +59,59 @@ fn handle_command(com: &str){
     //Create Identity command
     else if com == "1" {
         println!("Create Identity");
-        create_identity();
-        println!("Identity created successfully");
+        unsafe {
+            if crypto::get_id_masterkey() != None {
+                println!("Identity already exists. Overwrite? [y/N]");
+                let choice: String = read!("{}");
+                if choice != "y" && choice != "Y" {
+                    return;
+                }
+            }
+            crypto::create_identity();
+            println!("Identity created successfully");
+        }
     }
     //Create Keys for URL
     else if com == "2" {
-        println!("Create Keys for URL");
+        println!("Enter URL to create keys: ");
+        let url: String = read!();
+        let domain = http::parse_domain(&*url);
+        if domain == "Error" {
+            println!("Not valid sqrl:// URL");
+            return;
+        }
+        unsafe {
+            let imk = crypto::get_id_masterkey();
+            if imk == None {
+                println!("Please create an Identity first");
+                return;
+            }
+            // per-site public/private keys derived from the imk and the domain from the sqrl URL
+            let key_pair = crypto::create_keypair(imk.unwrap(), domain);
+            println!("PubKey: {:?}", key_pair.0);
+            println!("SecKey: {:?}", key_pair.1);
+        }
+
+        // if you would like to test the parsing functions simply uncomment these lines, recompile,
+        // and run the executable. feel free to add more test cases in these functions
+        //test_parse_nut();
+        //test_parse_domain();
     }
     //Enter password to view saved urls
     else if com == "3" {
         println!("Enter password to view saved urls");
     }
-
 }
 
-fn start_server() {
-    let server = Server::http("0.0.0.0:25519").unwrap();
-
-    for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}", request.method(), request.url());
-
-        let response = Response::from_string("www.google.com")
-            .with_status_code(301);
-        // .with_header(Header::from_bytes("Location", "www.google.com").unwrap());
-
-        request.respond(response);
-        // exit(0);
-    }
-}
-
-fn create_identity() {
-    //256 bit value, generated by using sha256 encryption on a randomly selected 32 bit value
-    let iuk: Digest = create_iuk();
-    println!("{:?}", iuk);
-    //identity masterkey is essentially an encrypted version of the iuk
-    let imk: Digest = sqrl_enhash(iuk);
-    println!("{:?}", imk);
-
-    //per-site public/private keys derived from the imk and the domain from the sqrl URL
-    let key_pair = create_keypair(imk, parse_domain(TEST_URL));
-    println!("{:?}", key_pair);
-
-    // if you would like to test the parsing functions simply uncomment these lines, recompile,
-    // and run the executable. feel free to add more test cases in these functions
-    //test_parse_nut();
-    //test_parse_domain();
-}
-
-// Identity Unlock Key
-// SQRL_Cryptography.pdf recommends to create the IUK by harvesting system
-// entropy into a SHA512 (or SHA256 - it is unclear if there is a mistake)
-// hash in order to create 256 bits of high quality entropy
-fn create_iuk() -> Digest {
-    // for now just requesting 32 random bytes to shove into SHA256
-    let rand: Vec<u8> = randombytes::randombytes(32);
-    return sha256::hash(&rand);
-}
-
-// SQRL EnHash function. generates what will be the identity master key
-// to ensure with complete certainty that our function is one-way, we SHA256
-// hash the input 16 times, each time XORing the output with previous output
-fn sqrl_enhash(input: Digest) -> Digest {
-    // do first-pass of SHA256
-    let mut digest = sha256::hash(input.as_ref());
-    // convert to vector to be able to index in
-    let mut hashed = digest.as_ref().to_vec();
-    let mut xorsum = digest.as_ref().to_vec();
-    // do another 15 iterations
-    for _ in 0..15 {
-        digest = sha256::hash(&hashed);
-        hashed = digest.as_ref().to_vec();
-        for i in 0..xorsum.len() {
-            // manual bitwise XOR on output
-            xorsum[i] = xorsum[i] ^ hashed[i];
+// can use this once we figure out how to either do IPC or send kill
+// signal to already running non-child process
+fn check_processes() {
+    let mut system = sysinfo::System::new_all();
+    // Need to update all information of system struct
+    system.refresh_all();
+    for (pid, proc_) in system.get_processes() {
+        if proc_.name().starts_with("rust-sqrl-client") {
+            println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
         }
     }
-    return Digest::from_slice(&xorsum).unwrap();
 }
-
-// generate the per site public/private keypair using the identity master key and the SQRL domain
-// (supplied by the SQRL server and parsed from the SQRL url by us)
-fn create_keypair(imk: Digest, domain: String) -> (PublicKey, SecretKey)
-{
-    let domain_as_input = domain.as_bytes();
-    let key = &auth::hmacsha256::Key::from_slice(imk.as_ref()).unwrap();
-
-    // crypto::auth::hmacsha256 with domain as input, identity master key as the key, used to
-    // generate a seed for the function used to generate the keypair
-    // described in  SQRL crypto document pg 10
-    let seed = &sign::ed25519::Seed::from_slice(
-        auth::hmacsha256::authenticate(domain_as_input, key).as_ref()
-    ).unwrap();
-
-    // seed is used to generate the keypair, using crypto::sign::keypair_from_seed
-    // also described in SQRL crypto doc pg 10
-    let key_pair = sign::keypair_from_seed(seed);
-
-    return key_pair;
-}
-
-// using the derived per site private key, creates a signature to be appended to all http queries
-// with the SQRL server the SQRL server can verify this signature using the paired public key
-// (user identity) sent with the request
-fn sign_url(url: &str, key: SecretKey) -> Vec<u8>
-{
-    let input = url.strip_prefix("sqrl://").unwrap().as_bytes();
-
-    //use the crypto::sign fnxn as described in the SQRL crypto doc pg 10
-    return sign::sign(input, &key);
-}
-
-
-//Parsing function to get back just the 'nut' portion of the URL
-fn parse_nut(url: &str) -> String
-{
-    let mut nut: String = String::from("");
-    if url.starts_with("sqrl://")
-    {
-        let mut temp_url = url.strip_prefix("sqrl://").unwrap();
-
-        temp_url = temp_url.split("?").collect::<Vec<&str>>()[1];
-
-        temp_url = temp_url.split("nut=").collect::<Vec<&str>>()[1];
-
-        temp_url = temp_url.split("&can=").collect::<Vec<&str>>()[0];
-
-        nut.push_str(temp_url);
-
-        return nut;
-    } else {
-        nut.push_str("Error");
-        return nut;
-    }
-}
-
-fn test_parse_nut()
-{
-    let url = "sqrl://sqrl.grc.com/cli.sqrl?nut=jLUOj4v1HsZm&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vZGVtbw";
-    assert_eq!("jLUOj4v1HsZm", parse_nut(&url));
-}
-
-// Parsing function to get back just the domain portion of the URL
-// the exact domain is required to compute the per site private key
-// we can fix formatting on the fnxn to get rid of empty lines, I just like to have my code spread
-// out and separated so its easy to read
-fn parse_domain(url: &str) -> String
-{
-    // url param is the SQRL url that looks like: sqrl://example.com/jimbo/?x=6&nut=...
-    // need to parse out the "example.com/jimbo"
-
-    if url.starts_with("sqrl://")
-    {
-        let mut domain: String = String::from("");
-        let mut temp_url = url.strip_prefix("sqrl://").unwrap();
-        let offset = determine_offset(temp_url.split("?").collect::<Vec<&str>>()[1]);
-
-        temp_url = temp_url.split("?").collect::<Vec<&str>>()[0];
-
-        if temp_url.find("@") != None
-        {
-            // keep things to the right of the @ symbol
-            temp_url = temp_url.split("@").collect::<Vec<&str>>()[1];
-        }
-
-        // domain ends at the beginning of the first '/' symbol, eg example.com/
-        let mut end_index = temp_url.find("/").unwrap() as u8;
-        // unless theres an extension, which is then given by the offset var
-        let mut index = 0;
-        for character in temp_url.chars()
-        {
-            if index == (end_index + offset) || character == '?'
-            {
-                break;
-            } else if index < end_index && (character.is_alphabetic() || character == '.')
-            {
-                domain.push(character.to_ascii_lowercase());
-            } else if index >= end_index && index < (end_index + offset)
-            {
-                domain.push(character);
-            }
-
-            index += 1;
-        }
-
-        return domain;
-    } else {
-        return String::from("Error");
-    }
-}
-
-//Helper function for the parsers
-fn determine_offset(params: &str) -> u8
-{
-    // given something like
-    // x=5&nut=oOB4QOFJux5Z&can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vYWNjb3VudC9jb25uZWN0ZWQtYWNjb3VudHMv
-
-    if params.get(0..1).unwrap() == "x"
-    {
-        let mut num_str = params.split("&").collect::<Vec<&str>>()[0];
-        num_str = num_str.split("=").collect::<Vec<&str>>()[1];
-
-        return num_str.parse::<u8>().unwrap();
-    }
-    return 0;
-}
-
-
-fn test_parse_domain()
-{
-    // test cases taken from GRCs SQRL Operating Details pg 7
-    // to lowercase
-    assert_eq!(String::from("example.com"), parse_domain("sqrl://ExAmPlE.cOm/?nut="));
-    // removing specified port num
-    assert_eq!(String::from("example.com"), parse_domain("sqrl://example.com:44344/?nut="));
-    // removing username prefix
-    assert_eq!(String::from("example.com"), parse_domain("sqrl://jonny@example.com/?nut="));
-    // removing username:pass prefix
-    assert_eq!(String::from("example.com"), parse_domain("sqrl://Jonny:Secret@example.com/?nut="));
-    // keeping extended auth domain
-    assert_eq!(String::from("example.com/jimbo"), parse_domain("sqrl://example.com/jimbo/?x=6&nut="));
-    // stopping at ? and only making domain lowercase, not extended auth
-    assert_eq!(String::from("example.com/JIMBO"), parse_domain("sqrl://EXAMPLE.COM/JIMBO?x=16&nut="));
-
-    assert_eq!(String::from("sqrl.grc.com/demo"), parse_domain("sqrl://steve:badpass@SQRL.grc.com:8080/demo/cli.sqrl?x=5&nut=oOB4QOFJux5Z&
-can=aHR0cHM6Ly9zcXJsLmdyYy5jb20vYWNjb3VudC9jb25uZWN0ZWQtYWNjb3VudHMv"));
-}
-
-
-// Sample http request function, will use later
-// fn send_request(url: &str) -> Result<(), ureq::Error> {
-//     let body: String = ureq::get(url)
-//         .set("Example-Header", "header value")
-//         .call()?
-//         .into_string()?;
-//     println!("{:?}", body);
-//     Ok(())
-// }
