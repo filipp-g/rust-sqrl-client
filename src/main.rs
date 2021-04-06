73214afac7f0cce52080a262c210030373e5a67e
Filipp G <8738425+filipp-g@users.noreply.github.com>
2021-04-05 21:53:02 -0400

    fix bug in reading user choice

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn main() {
     loop {
         println!("Enter in a command. Type 'h' for help with commands");
         //Read input from the user
-        let mut line: String = read!("{}\n");
-        let mut command: bool = false;
+        let mut line: String = read!();
+        let mut command: bool = true;
         //checks over the array to see if it's a valid command
         for x in 0..arr.len() {
             if arr[x] == line {
@@ -62,14 +62,14 @@ fn handle_command(com: &str) {
         unsafe {
             if crypto::get_id_masterkey() != None {
                 println!("Identity already exists. Overwrite? [y/N]");
-                let choice: String = read!("{}");
+                let choice: String = read!();
                 if choice != "y" && choice != "Y" {
                     return;
                 }
             }
-            crypto::create_identity();
-            println!("Identity created successfully");
         }
+        crypto::create_identity();
+        println!("Identity created successfully");
     }
     //Create Keys for URL
     else if com == "2" {
@@ -91,11 +91,6 @@ fn handle_command(com: &str) {
             println!("PubKey: {:?}", key_pair.0);
             println!("SecKey: {:?}", key_pair.1);
         }
-
-        // if you would like to test the parsing functions simply uncomment these lines, recompile,
-        // and run the executable. feel free to add more test cases in these functions
-        //test_parse_nut();
-        //test_parse_domain();
     }
     //Enter password to view saved urls
     else if com == "3" {
